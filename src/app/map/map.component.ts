a638607f848d7450d06402ad1a1ef9f3efb0a00a
Daniel Munger and Luke Philips <daniel+luke@>
2017-01-24 14:33:49 -0800

    map works with zoom

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Destination } from '../destination.model';
+import { SebmGoogleMap } from 'angular2-google-maps/core';
+
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
+//   directives: [SebmGoogleMap],
+//   styles: [`
+//    .sebm-google-map-container {
+//      height: 100px;
+//    }
+// `],
 })
 export class MapComponent implements OnInit {
   @Input() cityToDisplay: Destination;
@@ -12,6 +20,9 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  test(){
+    // SebmGoogleMap.backgroundColor = red;
   }
 
 }
